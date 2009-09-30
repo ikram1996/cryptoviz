6b2f91774842c99744a60563ab32daf274fbf609
yoyo23451 <yoyo23451@a3b1e438-a3a0-11de-ba6e-69e41c8db662>
2009-09-30 17:55:09 +0000

    update

--- trunk/SDES.java
+++ trunk/SDES.java
@@ -7,40 +7,21 @@ public class SDES
 
 
 
-	//todo: s-boxes
-
-
+	
 	private static int[] IP_Map = {2, 6, 3, 1, 4, 8, 5, 7};
 
 	private static int[] IPinverse_Map = {4, 1, 3, 5, 7, 2, 8, 6};
+//Commented out lines are todo
+	//private static int[] PC1_Map = {};
 
-	private static int[] PC1_Map = {57, 49, 41, 33, 25, 17,  9,
-					   1, 58, 50, 42, 34, 26, 18,
-					   10,  2, 59, 51, 43, 35, 27,
-					   19, 11,  3, 60, 52, 44, 36,
-					   63, 55, 47, 39, 31, 23, 15,
-					    7, 62, 54, 46, 38, 30, 22,
-					   14,  6, 61, 53, 45, 37, 29,
-					   21, 13,  5, 28, 20, 12,  4};
+	//private static int[] PC2_Map = {};
 
-	private static int[] PC2_Map = {14, 17, 11, 24,  1,  5,
-					   3, 28, 15,  6, 21, 10,
-					  23, 19, 12,  4, 26,  8,
-					  16,  7, 27, 20, 13,  2,
-					  41, 52, 31, 37, 47, 55,
-					  30, 40, 51, 45, 33, 48,
-					  44, 49, 39, 56, 34, 53,
-					  46, 42, 50, 36, 29, 32};
+	//private static int[] E_Map = {};
 
-	private static int[] E_Map = {32,  1,  2,  3,  4,  5,  4,  5,  6,  7,  8,  9,  8,  9, 10, 11,
-					12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21,
-					22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32,  1};
-
-	private static int[] P_Map = {16,  7, 20, 21, 29, 12, 28, 17,  1, 15, 23, 26,  5, 18, 31, 10,
-					 2,  8, 24, 14, 32, 27,  3,  9, 19, 13, 30,  6, 22, 11,  4, 25};
+	//private static int[] P_Map = {};
 
 
-
+//using John's functions (Should work the same)
 	public static boolean[] Permute(boolean[] in, int[] map)
 	{
 		int insize = in.length;
