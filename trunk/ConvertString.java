73d3c5c3641e307ccfdf7b08c45a5677f5e8a597
yoyo23451 <yoyo23451@a3b1e438-a3a0-11de-ba6e-69e41c8db662>
2009-10-05 17:53:26 +0000

    convertupdate

--- trunk/ConvertString.java
+++ trunk/ConvertString.java
@@ -3,6 +3,7 @@
 
 import java.awt.*;
 import  java.io.*;
+import java.util.*;
 
 public class ConvertString{
 
@@ -20,11 +21,85 @@ public class ConvertString{
 		return binaryString;
 	}
 
-	/*
+	public static String stringToAscii(String input)
+	{//will truncate to 64 bit
+		String output = "";		
+		for(int i =0; i<8; i++)//for each letter in the 64 bits
+		{
+			char letter = input.charAt(i);
+			//System.out.println((int)letter);
+			int ascii = (int)letter;
+			for(int j = 0; j<8; j++)//for each 8 bits in the character
+			{
+				if(j==0){if(ascii > 128)}
+				if(j==1){}
+				if(j==2){}
+				if(j==3){}
+				if(j==4){}
+				if(j==5){}
+				if(j==6){}
+				if(j==7){}
+			}
+		}		
+		return	output;
+	}
+
+	public static BitSet asciiToBinary(String binary)
+	{
+		BitSet bits = new BitSet(64);
+		for(int i = 0; i<64; i++)
+		{
+			if(i >= binary.length())
+			{
+				System.out.println("Debug: string too short");
+				break;
+			}
+			
+			if(binary.charAt(i) == '1')
+			{
+				bits.set(i, true);
+				//System.out.print(bits.get(i));
+			}
+		}
+		return bits;
+		
+
+
+
+
+		/*int character=0;
+		int index = 0;
+		boolean[] out= new boolean[64];	
+		for(int i = 0; i < 8; i++)
+		{	if(i >= input.length())
+			{	System.out.println("Debug: string too short");
+				break;}
+			
+			character = input.charAt(i);
+			for(int j=0; j<8; j++)
+			{	if((character - Math.pow(2,7-j)) > 0)
+				{	out[index] = true;
+					index++;
+					character = character - (int)Math.pow(2,7-j);}
+				else
+				{out[index] = false;		
+					index++;}}}
+		return out;*/}
+
+	
 	public static void main(String[] args){
 
 		String test = "hello world";
 		System.out.println(stringToBinary(test));
+		for(int i =0; i<64; i++)
+		{
+			if(asciiToBinary(stringToBinary(test)).get(i)==true)
+			System.out.print(1);
+			else
+			System.out.print(0);
+		}
+		System.out.println();
+		stringToAscii(test);
 	}
-	*/
+	
 }
