21c7102a99086bb7bd465bca3cdaff3f8ed022bb
fsrowe <fsrowe@a3b1e438-a3a0-11de-ba6e-69e41c8db662>
2009-10-05 16:06:57 +0000

    convertString class also gui work

--- CryptMain.java
+++ CryptMain.java
@@ -8,14 +8,20 @@ import java.util.Random;
 import  java.io.*;
 
 
-class CryptMain extends JFrame
+class CryptMain extends JFrame implements ActionListener
 {
 
 	File input;
 	JTextArea plainTextArea;
 	JTextArea binaryTextArea;
+	private JScrollPane jScrollPane1;
+	private JScrollPane jScrollPane2;
 	JPanel panel;
 	private final static String newline = "\n";
+	final JFileChooser fc = new JFileChooser();
+	protected JButton b1, b2, b3;
+
+
 	String line;
 	String binaryLine;
 
@@ -24,7 +30,7 @@ class CryptMain extends JFrame
 		setTitle("CryptoViz");	
 		
 		//Create a file chooser
-		final JFileChooser fc = new JFileChooser();
+		
 
 
 		// Creates a menubar for a JFrame
@@ -63,6 +69,8 @@ class CryptMain extends JFrame
 		editMenu.addSeparator();
 		editMenu.add(checkAction);
 
+
+		/*
 		// Add a listener to the New menu item. actionPerformed() method will
 		// invoked, if user triggred this menu item
 		newAction.addActionListener(new ActionListener() {
@@ -73,7 +81,112 @@ class CryptMain extends JFrame
 
 		openAction.addActionListener(new ActionListener() {
 		    public void actionPerformed(ActionEvent arg0) {
-		        int returnVal = fc.showOpenDialog(CryptMain.this);
+		        
+		    }
+		});
+		*/
+
+		
+		panel = new JPanel();
+		panel.setSize(200,200);
+		panel.setBackground(Color.gray);
+		panel.setVisible(true);
+
+/* THIS DOESN'T WORK LOLOL
+		   GroupLayout layout = new GroupLayout(panel);
+		   panel.setLayout(layout);
+		 
+		   // Turn on automatically adding gaps between components
+		   layout.setAutoCreateGaps(true);
+		 
+		   // Turn on automatically creating gaps between components that touch
+		   // the edge of the container and the container.
+		   layout.setAutoCreateContainerGaps(true);
+		 
+		   // Create a sequential group for the horizontal axis.
+		 
+		   GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
+		 
+		   // The sequential group in turn contains two parallel groups.
+		   // One parallel group contains the labels, the other the text fields.
+		   // Putting the labels in a parallel group along the horizontal axis
+		   // positions them at the same x location.
+		   //
+		   // Variable indentation is used to reinforce the level of grouping.
+		   hGroup.addGroup(layout.createParallelGroup().
+			    addComponent(b1).addComponent(b2));
+		   hGroup.addGroup(layout.createParallelGroup().
+			    addComponent(plainTextArea).addComponent(binaryTextArea));
+		   layout.setHorizontalGroup(hGroup);
+		   
+		   // Create a sequential group for the vertical axis.
+		   GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
+		 
+		   // The sequential group contains two parallel groups that align
+		   // the contents along the baseline. The first parallel group contains
+		   // the first label and text field, and the second parallel group contains
+		   // the second label and text field. By using a sequential group
+		   // the labels and text fields are positioned vertically after one another.
+		   vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).
+			    addComponent(b1).addComponent(plainTextArea));
+		   vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).
+			    addComponent(b2).addComponent(binaryTextArea));
+		   layout.setVerticalGroup(vGroup);
+*/
+
+		b1 = new JButton("Input Plain Text");
+		b1.setVerticalTextPosition(AbstractButton.CENTER);
+		b1.setHorizontalTextPosition(AbstractButton.LEADING); //aka LEFT, for left-to-right locales
+		b1.setMnemonic(KeyEvent.VK_D);
+		b1.setEnabled(true);
+		b1.addActionListener(this);
+		panel.add(b1);
+
+		b2 = new JButton("Convert to Binary");
+		b2.setVerticalTextPosition(AbstractButton.CENTER);
+		b2.setHorizontalTextPosition(AbstractButton.LEADING); //aka LEFT, for left-to-right locales
+		b2.setMnemonic(KeyEvent.VK_D);
+		b2.addActionListener(this);
+		b2.setEnabled(false);
+		panel.add(b2);
+
+		
+
+
+		plainTextArea = new JTextArea();
+		plainTextArea.setColumns(20);
+		plainTextArea.setLineWrap(true);
+		plainTextArea.setRows(10);
+		plainTextArea.setWrapStyleWord(true);
+		jScrollPane1 = new JScrollPane(plainTextArea); 
+		plainTextArea.setEditable(true);
+
+		binaryTextArea = new JTextArea();
+		binaryTextArea.setColumns(20);
+		binaryTextArea.setLineWrap(true);
+		binaryTextArea.setRows(10);
+		binaryTextArea.setWrapStyleWord(true);
+		jScrollPane2 = new JScrollPane(binaryTextArea); 
+		binaryTextArea.setEditable(true);
+
+		//panel.add(plainTextArea);
+		//panel.add(binaryTextArea);
+
+		add(panel);
+
+	}
+
+	public void actionPerformed(ActionEvent evt){
+
+		Object source = evt.getSource();
+
+		if(source == b1) getPlainText();
+		if(source == b2) outputBinary();
+
+	}
+
+	public void getPlainText(){
+		int returnVal = fc.showOpenDialog(CryptMain.this);
 
 			    //Process the results.
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
@@ -83,29 +196,16 @@ class CryptMain extends JFrame
 			readFile(input);
 			//Reset the file chooser for the next time it's shown.
 			fc.setSelectedFile(null);
-		    }
-		});
-
-		panel = new JPanel();
-		panel.setSize(200,200);
-		panel.setBackground(Color.black);
-		panel.setVisible(true);
-
 		
+		b2.setEnabled(true);
+	}
 
-		plainTextArea = new JTextArea(40, 40);
-		JScrollPane scrollPane = new JScrollPane(plainTextArea); 
-		plainTextArea.setEditable(true);
-
-		binaryTextArea = new JTextArea(40, 40);
-		JScrollPane scrollPane2 = new JScrollPane(binaryTextArea); 
-		binaryTextArea.setEditable(true);
-
-		panel.add(plainTextArea);
-		panel.add(binaryTextArea);
-
-		add(panel);
-
+	public void outputBinary(){
+	
+		String plain = plainTextArea.getText();
+		binaryLine = ConvertString.stringToBinary(line);
+		binaryTextArea.append(binaryLine + newline);
+		
 	}
 
 	public void readFile(File input){
@@ -128,10 +228,8 @@ class CryptMain extends JFrame
 
 		      // this statement reads the line from the file and print it to
 			// the console.
-			line = dis.readLine();
-			binaryLine = ConvertString.stringToBinary(line);
+			line = dis.readLine();		
 			plainTextArea.append(line + newline);
-			binaryTextArea.append(binaryLine + newline);
 		      }
 
 		      // dispose all the resources after using them.
@@ -151,7 +249,7 @@ class CryptMain extends JFrame
 		CryptMain crypt = new CryptMain();
 		//frame.setContentPane(menu.createContentPane());
         	crypt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        crypt.setSize(600,800);
+	        crypt.setSize(1000,1000);
 
 		//place frame in middle of screen
 		Toolkit toolkit = Toolkit.getDefaultToolkit();  
