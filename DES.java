02f8a7906dadf30ae6965da77177dcd35b763f0b
fsrowe <fsrowe@a3b1e438-a3a0-11de-ba6e-69e41c8db662>
2009-11-10 19:40:17 +0000

    keyframe

--- DES.java
+++ DES.java
@@ -57,9 +57,9 @@ public class DES
 
 	//left shift of two 28-bit blocks at the same time -- used for subkey generation
 	public final static int[] LS_Map =  { 2,  3,  4,  5,  6,  7,  8,
-					   9, 10, 11, 12, 13, 14, 15,
-					  16, 17, 18, 19, 20, 21, 22,
-					  23, 24, 25, 26, 27, 28,  1,
+					   	9, 10, 11, 12, 13, 14, 15,
+						16, 17, 18, 19, 20, 21, 22,
+						23, 24, 25, 26, 27, 28,  1,
 
 					  30, 31, 32, 33, 34, 35, 36,
 					  37, 38, 39, 40, 41, 42, 43,
